855cbe3db75d277574d1121873e35279966cad82
conner fortner <fortner.conner@yahoo.com>
2020-10-11 22:57:44 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -40,4 +40,4 @@ def select_character_names_and_number_of_books_they_are_in
   FROM character_books
   JOIN characters ON character_books.character_id = characters.id
   GROUP BY characters.name ORDER BY book_count DESC, characters.name ASC;"
-end74
+end
